c93b9d25a44ee05a06c89b75b5b9119eef43e5fb
快乐编程人 <happycoding996@gmail.com>
2023-06-11 10:11:02 +0800

    Update index.js

--- api/index.js
+++ api/index.js
@@ -7,11 +7,12 @@ class Api{
 	 * Get同步请求
 	 * @param data 带参数
 	 */
-	async getAsync(url, data) {  
+	async getAsync(url, data, header={}) {  
 		return await asyncRequest({
 			url: url,
 			method: "GET",
-			data: data
+			data: data,
+			header: header
 		})
 	}
 	
@@ -19,11 +20,25 @@ class Api{
 	 * Get请求
 	 * @param data 带参数
 	 */
-	get(url, data) {  
+	get(url, data, header={}) {  
 		return request({
 			url: url,
 			method: "GET",
-			data: data
+			data: data,
+			header: header
+		})
+	}
+	
+	/**
+	 * Post同步请求
+	 * @param data 带参数
+	 */
+	async postAsync(url, data, header={}) {  
+		return await asyncRequest({
+			url: url,
+			method: "POST",
+			data: data,
+			header: header
 		})
 	}
 
@@ -31,11 +46,12 @@ class Api{
 	 * Post请求
 	 * @param data 带参数
 	 */
-	post(url, data) {  
+	post(url, data, header={}) {  
 		return request({
 			url: url,
 			method: "POST",
-			data: data
+			data: data,
+			header: header
 		})
 	}
 	
@@ -43,10 +59,12 @@ class Api{
 	 * 上传文件
 	 * @param filePath 文件地址
 	 */
-	uploadFile(url, filePath){
+	uploadFile(url, filePath, data, header={}){
 		return request({
 			url: url,
-			filePath: filePath
+			filePath: filePath,
+			data: data,
+			header: header
 		})
 	}
 
